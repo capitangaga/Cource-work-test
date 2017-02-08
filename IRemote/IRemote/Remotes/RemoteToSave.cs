4188d2a7b750b608dc80e5a23a2623f9149a48e3
capitangaga <kirillgolowko@yandex.ru>
2017-02-08 12:49:10 +0300

    Added class to save remotes

--- IRemote/IRemote/Remotes/RemoteToSave.cs
+++ IRemote/IRemote/Remotes/RemoteToSave.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using SQLite;
+
 namespace IRemote
 {
+	/// <summary>
+	/// Remote to save in SQLite db
+	/// </summary>
 	public class RemoteToSave
 	{
-		public RemoteToSave()
-		{
-		}
+		[PrimaryKey, AutoIncrement]
+		/// <summary>
+		/// Gets or sets the identifier of Remote
+		/// </summary>
+		/// <value>The identifier.</value>
+		public int ID { get; set; }
+		/// <summary>
+		/// Gets or sets the name of Remote
+		/// </summary>
+		/// <value>The name.</value>
+		public string Name { get; set; }
+		/// <summary>
+		/// Gets or sets the string with keys commands.
+		/// </summary>
+		/// <value>The keys.</value>
+		public string KeysCommands { get; set; }
+		/// <summary>
+		/// Gets or sets the string with keys textss.
+		/// </summary>
+		/// <value>The keys texts.</value>
+		public string KeysTexts { get; set; }
+
 	}
 }
