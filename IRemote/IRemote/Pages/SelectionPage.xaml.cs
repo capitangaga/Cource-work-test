0e07ed041e64ff3b59896afefd380f089925e72e
capitangaga <kirillgolowko@yandex.ru>
2017-02-23 18:27:23 +0300

    db tested

--- IRemote/IRemote/Pages/SelectionPage.xaml.cs
+++ IRemote/IRemote/Pages/SelectionPage.xaml.cs
@@ -29,5 +29,15 @@ namespace IRemote
 				RemotesList.ItemsSource = await App.Database.GetRemotesWithCategoryAsync(BindingContext as string);
 			}
 		}
+		protected async void AddNewClicked(object sender, EventArgs e)
+		{
+			Remote newRemote = new Remote
+			{
+				Name = "New Remote",
+				Category = BindingContext == null ? "" : BindingContext as string
+			};
+			await App.Database.SaveRemoteAsync(newRemote);
+			// TODO Navigation to new Remote
+		}
 	}
 }
